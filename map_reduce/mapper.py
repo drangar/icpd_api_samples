30cca3d23d9651d5f7e8ba8623f4f2f892bbb4f4
Deepak Rangarao <drangar@us.ibm.com>
2018-11-01 18:13:50 -0700

    Added folders and initial files for lifecycle management

--- map_reduce/mapper.py
+++ map_reduce/mapper.py
@@ -1,1 +1,10 @@
-testing
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    unpacked = line.split(",")
+    stadium, capacity, expanded, location, surface, turf, team, opened, weather, roof, elevation = line.split(",")
+    # results = [turf, "1"]
+    results = [stadium+":"+location]
+    print("\t".join(results))
+
